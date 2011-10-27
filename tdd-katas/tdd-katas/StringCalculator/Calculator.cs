1a3ead81b5d41080ce65a84bf9d6f9ea57ae865e
Joseph Bulger <joseph@josephbulger.com>
2011-10-27 10:36:09 -0400

    test 3 refactored

--- tdd-katas/tdd-katas/StringCalculator/Calculator.cs
+++ tdd-katas/tdd-katas/StringCalculator/Calculator.cs
@@ -7,7 +7,30 @@ namespace tdd_katas.StringCalculator
     {
         public static int Add(string input)
         {
-            return string.IsNullOrEmpty(input) ? 0 : input.Contains(",") ? input.Split(",".ToCharArray()).Sum(number => ParseSingleNumber(number)) : ParseSingleNumber(input);
+            return string.IsNullOrEmpty(input) ? 0 : ParseInput(input);
+        }
+
+        private static int ParseInput(string input)
+        {
+            return ContainsAny(input, GetPossibleDelimiters()) ? ParseMultipleNumbers(input) : ParseSingleNumber(input);
+        }
+
+        private static string GetPossibleDelimiters()
+        {
+            return ",";
+        }
+
+        private static bool ContainsAny(string input, string getPossibleDelimiters)
+        {
+            return getPossibleDelimiters.ToCharArray().Any(input.Contains);
+        }
+
+        private static int ParseMultipleNumbers(string input)
+        {
+            var numbers = input.Split(GetPossibleDelimiters().ToCharArray());
+
+            var result = numbers.Sum(number => ParseSingleNumber(number));
+            return result;
         }
 
         private static int ParseSingleNumber(string input)
