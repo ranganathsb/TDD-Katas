0f98ea4596f8ca1a1649d1a66e1e2c756c142770
Joseph Bulger <joseph@josephbulger.com>
2011-10-27 10:27:59 -0400

    first test

--- tdd-katas/tdd-katas/StringCalculator/Calculator.cs
+++ tdd-katas/tdd-katas/StringCalculator/Calculator.cs
@@ -1,7 +1,15 @@
-﻿namespace tdd_katas.StringCalculator
+﻿using System;
+using System.Linq;
+
+namespace tdd_katas.StringCalculator
 {
     public class Calculator
     {
-         
+        public static int Add(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        
     }
 }
--- tdd-katas/tdd-katas/StringCalculator/Tests.cs
+++ tdd-katas/tdd-katas/StringCalculator/Tests.cs
@@ -1,7 +1,19 @@
-﻿namespace tdd_katas.StringCalculator
+﻿using System;
+using NUnit.Framework;
+
+namespace tdd_katas.StringCalculator
 {
+    [TestFixture]
     public class Tests
     {
-         
+        [TestCase("")]
+        [TestCase(null)]
+        [Test]
+        public void Add_WhenGivenEmptyStringOrNull_ReturnsZero(string input)
+        {
+            var result = Calculator.Add(input);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
     }
 }
