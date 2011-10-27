971c56d404b2ed1df3fcc9ac3f625bbb794f07b6
Joseph Bulger <joseph@josephbulger.com>
2011-10-27 10:32:59 -0400

    test 3 green

--- tdd-katas/tdd-katas/StringCalculator/Calculator.cs
+++ tdd-katas/tdd-katas/StringCalculator/Calculator.cs
@@ -7,7 +7,7 @@ namespace tdd_katas.StringCalculator
     {
         public static int Add(string input)
         {
-            return string.IsNullOrEmpty(input) ? 0 : ParseSingleNumber(input);
+            return string.IsNullOrEmpty(input) ? 0 : input.Contains(",") ? input.Split(",".ToCharArray()).Sum(number => ParseSingleNumber(number)) : ParseSingleNumber(input);
         }
 
         private static int ParseSingleNumber(string input)
