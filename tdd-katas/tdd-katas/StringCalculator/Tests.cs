cd6d68b6f7c8f22e0426ff7af50bd720a074b4b7
josephbulger <joseph@josephbulger.com>
2011-11-01 08:54:50 -0400

    test 1 red

--- tdd-katas/tdd-katas/StringCalculator/Tests.cs
+++ tdd-katas/tdd-katas/StringCalculator/Tests.cs
@@ -6,6 +6,17 @@ namespace tdd_katas.StringCalculator
     [TestFixture]
     public class Tests
     {
+        [Test]
+        public void Add_WhenGivenEmptyString_ShouldReturnZero()
+        {
+            //Arrange
+            
+            //Act
+            var result = Calculator.Add(string.Empty);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(0));
+        }
         
     }
 }
